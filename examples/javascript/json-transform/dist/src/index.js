b228fc2afca557eaa43e8e7f06ba0eb563771373
miciav <5889596+miciav@users.noreply.github.com>
2026-04-21 12:20:40 +0200

    chore: ignore generated javascript artifacts

--- examples/javascript/json-transform/dist/src/index.js
+++
@@ -1,5 +0,0 @@
-import { createRuntime } from "nanofaas-function-sdk";
-import { handleJsonTransform } from "./handler.js";
-const runtime = createRuntime();
-runtime.register("json-transform", handleJsonTransform);
-await runtime.start();
--- examples/javascript/word-stats/dist/src/index.js
+++
@@ -1,5 +0,0 @@
-import { createRuntime } from "nanofaas-function-sdk";
-import { handleWordStats } from "./handler.js";
-const runtime = createRuntime();
-runtime.register("word-stats", handleWordStats);
-await runtime.start();
