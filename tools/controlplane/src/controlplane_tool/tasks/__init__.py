0f996c65c67df9beb0da49108bb7e0ed120a1807
miciav <5889596+miciav@users.noreply.github.com>
2026-05-06 13:46:33 +0200

    Add internal task model

---
+++ tools/controlplane/src/controlplane_tool/tasks/__init__.py
@@ -0,0 +1,13 @@
+from controlplane_tool.tasks.models import (
+    CommandTaskSpec,
+    ExecutionTarget,
+    TaskResult,
+    TaskStatus,
+)
+
+__all__ = [
+    "CommandTaskSpec",
+    "ExecutionTarget",
+    "TaskResult",
+    "TaskStatus",
+]
